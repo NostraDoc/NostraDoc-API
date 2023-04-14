b03b3ffcc91fb77e688620fdfa4c6cdf8887778f
MrLiam2614 <liamcominotti@gmail.com>
2023-04-14 22:30:39 +0200

    Initial Commit

---
+++ src/main/java/me/mrliam2614/net/nostradocapi/NostraDocApiApplication.java
@@ -0,0 +1,13 @@
+package me.mrliam2614.net.nostradocapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NostraDocApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NostraDocApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/mrliam2614/net/nostradocapi/NostraDocApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mrliam2614.net.nostradocapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NostraDocApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
